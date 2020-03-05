db0c165875446988ba419394c0d7614ce5e3e28d
sai nani <sai nani@LAPTOP-EK3HR2D1>
2020-03-05 22:46:17 +0530

    first commit

---
+++ epam/src/main/java/epam/Collection.java
@@ -0,0 +1,15 @@
+package epam;
+import java.util.ArrayList;
+public class Collection {
+	public static void main(String args[])
+	{
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("epam");
+		list.add("collection");
+		list.add("webinar");
+		for(String x: list)
+		{
+			System.out.print(x+" ");
+		}
+	}
+}
